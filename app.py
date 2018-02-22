5773d6a8b61ac7a78cba3c724772aa55d149b056
Pierre FRAYER <pfrayer@gmail.com>
2018-02-22 22:47:21 +0000

    See images in different ways

--- app.py
+++ app.py
@@ -9,19 +9,45 @@ app = Flask(__name__)
 def hello():
     return "Hello World!"
 
+#### Images
 @app.route("/images")
 def images():
-	images = []
-	for image in client.images.list():
-		images.append(image.attrs)
-	return jsonify(result=images)
+    images = {}
+    for image in client.images.list():
+        if image.attrs["RepoTags"]:
+            images[image.attrs["RepoTags"][0]] = image.attrs
+    return jsonify(result=images)
 
+@app.route("/images/dangling")
+def dangling_images():
+    images = []
+    for image in client.images.list(filters={'dangling': True}):
+        images.append(image.attrs)
+    return jsonify(result=images)
+
+@app.route("/images/all")
+def all_images():
+    images = []
+    for image in client.images.list(all=True):
+        images.append(image.attrs)
+    return jsonify(result=images)
+
+#### Volumes
+@app.route("/volumes")
+def volumes():
+    volumes = []
+    for config in client.volumes.list():
+        volumes.append(config.attrs)
+    return jsonify(result=volumes)
+
+#### Containers
 @app.route("/containers")
 def containers():
-	containers = []
-	for container in client.containers.list():
-		containers.append(container.attrs)
-	return jsonify(result=containers)
+    containers = []
+    for container in client.containers.list():
+        containers.append(container.attrs)
+    return jsonify(result=containers)
 
+#### Main
 if __name__ == '__main__':
     app.run(debug=True)
